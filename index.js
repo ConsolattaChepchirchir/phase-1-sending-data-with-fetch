36cc83d33846b3989c4943f84b984a861f56be4f
ConsolattaChepchirchir <consochep97@gmail.com>
2023-03-31 15:08:00 +0300

    complete fetch assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(name,email){
+  const personalData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(personalData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((data) => {
+      const givendata = data.id;
+      document.body.append(givendata)
+    })
+    .catch(error => {
+      let returnmessage = 'invalid data';
+      document.body.append(error.returnmessage);
+    }
+      )
+    }
+    submitData();
+    
